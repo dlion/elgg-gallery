1486bd7865fae4c57868c1c3d7cac8751bab6cf8
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-01-02 14:28:30 +0100

    Added language files

---
+++ languages/en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * English Language Pack
+ */
+
+$english = array(
+    //Error
+    'gallery:no:imageshow' => 'No image to show!',
+    'gallery:no:imagegallery' => 'No images in your gallery!',
+    // Title
+    'gallery:title:add' => 'Add Image',
+    'gallery:title:showall' => 'All images',
+    'gallery:title:showone' => "Show Image - %s",
+    'gallery:title:error' => "Error: %s",
+    //Status
+    'gallery:status:upsuccess' => 'Image uploaded successfull!',
+    //Action
+    'gallery:delete:link' => 'Delete image'
+);
+
+add_translation("en", $english);
---
+++ languages/it.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Italian Language Pack
+ */
+
+$italiano = array(
+    //Error
+    'gallery:no:imageshow' => 'Nessuna immagine da visualizzare!',
+    'gallery:no:imagegallery' => 'Nessuna immagine nella tua galleria!',
+    // Title
+    'gallery:title:add' => 'Aggiungi Immagine',
+    'gallery:title:showall' => 'Tutte le tue immagini',
+    'gallery:title:showone' => 'Visualizza immagine - ',
+    'gallery:title:error' => "Errore: %s",
+    // Status
+    'gallery:status:upsuccess' => 'Immagine caricata correttamente!',
+    // Action
+    'gallery:delete:link' => 'Cancella Immagine'
+);
+
+add_translation("it", $italiano);
