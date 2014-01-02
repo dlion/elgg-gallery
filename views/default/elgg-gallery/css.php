7edc03c6e354fbd5dd55c9c2e4b48d179228c63d
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-01-02 14:29:49 +0100

    Added style to view page

--- views/default/elgg-gallery/css.php
+++ views/default/elgg-gallery/css.php
@@ -16,3 +16,40 @@
     float: left;
     padding: 10px 10px 10px 10px;
 }
+
+/* Show Style in elgg-gallery/view */
+.titolone {
+    color: #8e44ad;
+    font-size: 60px;
+    padding:30px 30px 30px 30px;
+    font-weight: bold;
+}
+
+.media {
+    background-color: #34495e;
+    text-align: center;
+    padding-top: 30px;
+}
+
+.immagine-full {
+    border: 1px solid #ecf0f1;
+}
+
+.description {
+    width: auto;
+    border: 1px solid #ecf0f1;
+    padding-top: 20px;
+    font-size: 30px;
+    background-color: #ecf0f1;
+    padding-bottom: 10px;
+}
+
+.action {
+    background-color: #e74c3c;
+    width: 100px;
+    font-weight: bold;
+}
+
+.del:link {
+    color: #2c3e50;
+}
